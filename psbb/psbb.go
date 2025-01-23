858693e9162cd1ac05d2a7adb49a7c90e561d9a8
dwiw96 <dwiwahyudi1996@gmail.com>
2025-01-23 13:30:55 +0700

    Added feature psbb

---
+++ psbb/psbb.go
@@ -0,0 +1,31 @@
+package psbb
+
+import (
+	"fmt"
+)
+
+func MinimumBus(n int, members []int) {
+	passengerMap := make(map[int]int)
+	// resMap := make(map[int]bool)
+	var bus int
+
+	for _, v := range members {
+		passengerMap[v]++
+	}
+
+	for _, val := range members {
+		target := 4 - val
+		if passengerMap[val] <= 0 {
+			continue
+		}
+		if passengerMap[target] > 0 {
+			passengerMap[val]--
+			passengerMap[target]--
+		} else {
+			passengerMap[val]--
+		}
+		bus++
+	}
+
+	fmt.Printf(">> Minimum bus required is: %d", bus)
+}
